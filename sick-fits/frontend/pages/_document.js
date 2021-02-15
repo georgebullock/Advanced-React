452552ddf0f5ab7bd25d3dd353482ed37253561c
George <george.bullock@gmx.de>
2021-02-15 21:34:44 +0100

    Module-9: Remove comments

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -8,11 +8,7 @@ class AppDocument extends Document {
     const page = renderPage((App) => (props) =>
       sheet.collectStyles(<App {...props} />)
     );
-
     const styleTags = sheet.getStyleElement();
-
-    console.log('page: ', page);
-    console.log('styleTags: ', styleTags);
 
     return { ...page, styleTags };
   }
